b363f7eb0c7ac66cd2981061d8093ab356ec3339
arnime99 <arnaumerin@gmail.com>
2024-05-04 01:54:55 +0200

    Animatorr

---
+++ Assets/ParalaxMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxMovement : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] bool scrollLeft;
+
+    float textureWidht;
+    void Start()
+    {
+        TextureValues();
+        
+    }
+
+     void TextureValues()
+     {
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        textureWidht = sprite.texture.width / sprite.pixelsPerUnit;
+     }
+
+    void Scroll()
+    {
+        float velX = moveSpeed * Time.deltaTime;
+        transform.position += new Vector3(velX, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
